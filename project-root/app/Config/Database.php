6a6d951643b4175028bd0226a4c701fdb521fd3a
Phileo0604 <Phileo0604@gmail.com>
2023-06-22 22:07:05 +0800

    Added Form Library and Form Controller

--- project-root/app/Config/Database.php
+++ project-root/app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => '',
         'password' => '',
-        'database' => '',
+        'database' => APPPATH . 'Database',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
--- project-root/app/Controllers/FormController.php
+++ project-root/app/Controllers/FormController.php
@@ -3,37 +3,27 @@
 namespace App\Controllers;
 
 use App\Libraries\FormBuilder;
-
+use Config\Database;
+use CodeIgniter\Controller;
 class FormController extends BaseController
 {
-    protected $formbuilder;
-    public function __construct()
-    {
-        parent::__construct();
-        $this->formbuilder = new FormBuilder();
-    }
-
+    
+    // protected $formbuilder;
+    // public function __construct()
+    // {
+    //     parent::__construct();s
+    //     $this->formbuilder = new FormBuilder();
+    // }
+    
     public function index()
-    {
-        // new form
-        $this->formbuilder->assign_vars(
-            // table name
-            'myTableName',
-            // row id to populate form values
-            '23'
-        );
+        {
+            $db = Database::connect();
+            $formBuilder = new FormBuilder($db);
+            $form = $formBuilder->buildForm('example_form');
 
-        // exclude these values
-        $this->formbuilder->exclude_form_values(['timestamp', 'lastModifiedBy']);
-
-        // hide these values
-        $this->formbuilder->hide_form_values(['id']);
-
-        // build the table
-        $data['my_form'] = $this->formbuilder->build_form();
-
-        return view('myView', $data);
-    }
+            return view('forms', ['form' => $form]);
+        }
+    
 
     public function test()
     {
